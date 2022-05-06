740d9f36965c0d10a4d9c417b7ddd1816ab194be
alfazari <faa1909@outlook.com>
2022-05-06 14:04:23 +0700

    feat: add more about variable lesson

---
+++ week1/moreAboutVariable/arithmetic.py
@@ -0,0 +1,11 @@
+# Write your solution here
+x = 27
+y = 15
+a = x + y
+b = x - y
+c = x * y
+d = x / y
+print(f'27 + 15 = {str(a)}')
+print(f'27 - 15 = {str(b)}')
+print(f'27 * 15 = {str(c)}')
+print(f'27 / 15 = {str(d)}')
---
+++ week1/moreAboutVariable/extraSpace.py
@@ -0,0 +1,17 @@
+name = "Tim Tester"
+age = 20
+skill1 = "python"
+level1 = "beginner"
+skill2 = "java"
+level2 = "veteran"
+skill3 = "programming"
+level3 = "semiprofessional"
+lower = 2000
+upper = 3000
+
+print("my name is ", name, " , I am ", age, "years old")
+print("my skills are")
+print("- ", skill1, " (", level1, ")")
+print("- ", skill2, " (", level2, ")")
+print("- ", skill3, " (", level3, " )")
+print("I am looking for a job with a salary of", lower, "-", upper, "euros per month")
