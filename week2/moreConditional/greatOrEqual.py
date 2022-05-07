5d80d0cbaaa617b96a704d2b56fc72dcdcb0378d
alfazari <faa04807@gmail.com>
2022-05-07 12:33:30 +0700

    feat: add great or equal exercise

---
+++ week2/moreConditional/greatOrEqual.py
@@ -0,0 +1,9 @@
+# Write your solution here
+number1 = int(input('Please type in the first number:'))
+number2 = int(input('Please type in another number:'))
+if number1 == number2:
+    print('The number are equal!')
+elif number1 > number2:
+    print(f'The greater number was: {number1}')
+else:
+    print(f'The greater number was: {number2}')
