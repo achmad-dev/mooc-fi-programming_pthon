918c9b930daa1b796fdcdb06535f795a1e49bb9a
alfazari <faa04807@gmail.com>
2022-05-10 21:51:23 +0700

    add: exercise input validation

---
+++ week2/simpleLoops/inputValidation.py
@@ -0,0 +1,11 @@
+from math import sqrt
+# Write your solution here
+while True:
+    num = int(input('Please type in a number:'))
+    if num > 0:
+        print(sqrt(num))
+    elif num < 0:
+        print('Invalid number')
+    else:
+        break
+print('Exiting...')
